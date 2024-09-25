385c882c50989e1dea1a5f58c857752c99a203e1
Matías Barisone <matibarisone3@gmail.com>
2024-09-24 21:39:53 -0300

    FORMATO

--- server.js
+++ server.js
@@ -1,22 +1,19 @@
-const express = require('express');
-const { Employees } = require('./src/modelos/employees');
-const { Product } = require('./src/modelos/product');
-const { ProductCategoryView } = require('./src/modelos/productsandcategories');
-const { VistaOrderDetails } = require('./src/modelos/VistaOrderDetails');
+const express = require("express");
+const { Employees } = require("./src/modelos/employees");
+const { Product } = require("./src/modelos/product");
+const { ProductCategoryView } = require("./src/modelos/productsandcategories");
+const { VistaOrderDetails } = require("./src/modelos/VistaOrderDetails");
 
-
-const { sequelize } = require('./src/conexion/connection');
-const {Op} = require('sequelize');
+const { sequelize } = require("./src/conexion/connection");
+const { Op } = require("sequelize");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-
-
-
 // Conectar a la base de datos antes de iniciar el servidor
-sequelize.authenticate()
+sequelize
+  .authenticate()
   .then(() => {
-    console.log('Conexión establecida con éxito!');
+    console.log("Conexión establecida con éxito!");
 
     // Iniciar el servidor en el puerto definido solo después de que la conexión sea exitosa
     app.listen(PORT, () => {
@@ -24,25 +21,32 @@ sequelize.authenticate()
     });
   })
   .catch((error) => {
-    console.error('No me pude conectar porque sucedió: ', error);
+    console.error("No me pude conectar porque sucedió: ", error);
   });
 
 // Endpoint para obtener todos los productos
-app.get('/productos', async (req, res) => {
+app.get("/productos", async (req, res) => {
   try {
     const products = await Product.findAll(
       //ORDERNAR POR:
-      {order: [['CategoryID','ASC'],['productName','DESC']]}
+      {
+        order: [
+          ["CategoryID", "ASC"],
+          ["productName", "DESC"],
+        ],
+      }
     );
     res.json(products);
   } catch (error) {
-    console.error('Error al obtener los productos:', error);
-    res.status(500).json({ error: 'Ocurrió un error al obtener los productos' });
+    console.error("Error al obtener los productos:", error);
+    res
+      .status(500)
+      .json({ error: "Ocurrió un error al obtener los productos" });
   }
 });
 
 //VIEW CON MYSQL WORKBENCH DE ORDENES JUNTANDO DOS TABLAS
-app.get('/VistaOrderDetails/orders/:OrderID', async (req, res) => {
+app.get("/VistaOrderDetails/orders/:OrderID", async (req, res) => {
   try {
     const { orderId } = req.query; // Recibir el parámetro 'orderId' desde la URL
 
@@ -51,113 +55,121 @@ app.get('/VistaOrderDetails/orders/:OrderID', async (req, res) => {
       // Si se recibe un OrderID, aplicar el filtro y ordenar por ProductID de forma descendente
       orders = await VistaOrderDetails.findAll({
         where: { OrderID: orderId },
-        order: [['ProductID', 'DESC']] // Ordenar por ProductID descendente
+        order: [["ProductID", "DESC"]], // Ordenar por ProductID descendente
       });
     } else {
       // Si no se recibe un OrderID, traer todas las órdenes ordenadas por ProductID descendente
       orders = await VistaOrderDetails.findAll({
-        order: [['ProductID', 'DESC']] // Ordenar por ProductID descendente
+        order: [["ProductID", "DESC"]], // Ordenar por ProductID descendente
       });
     }
 
     // Enviar la respuesta con los resultados
     res.json(orders);
   } catch (error) {
-    console.error('Error al obtener las órdenes:', error);
-    res.status(500).send('Error al obtener las órdenes');
+    console.error("Error al obtener las órdenes:", error);
+    res.status(500).send("Error al obtener las órdenes");
   }
 });
 
 // BUSQUEDA POR PRODUCTOS Y CATEGORIAS
-app.get('/productsandcategories', async (req, res) => {
+app.get("/productsandcategories", async (req, res) => {
   try {
     const { productsandcategories } = req.params;
     const product = await Product.findAll(productsandcategories);
-    product ? res.json(product) 
-            : res.status(404).json({ message: 'Producto no encontrado' });
+    product
+      ? res.json(product)
+      : res.status(404).json({ message: "Producto no encontrado" });
   } catch (error) {
-    console.error('Error al obtener el producto:', error);
-    res.status(500).json({ error: 'Ocurrió un error al obtener el producto' });
+    console.error("Error al obtener el producto:", error);
+    res.status(500).json({ error: "Ocurrió un error al obtener el producto" });
   }
 });
 
 // BUSQUEDA POR ID
-app.get('/productos/:productID', async (req, res) => {
+app.get("/productos/:productID", async (req, res) => {
   try {
     const { productID } = req.params;
     const product = await Product.findByPk(productID);
-    product ? res.json(product) 
-            : res.status(404).json({ message: 'Producto no encontrado' });
+    product
+      ? res.json(product)
+      : res.status(404).json({ message: "Producto no encontrado" });
   } catch (error) {
-    console.error('Error al obtener el producto:', error);
-    res.status(500).json({ error: 'Ocurrió un error al obtener el producto' });
+    console.error("Error al obtener el producto:", error);
+    res.status(500).json({ error: "Ocurrió un error al obtener el producto" });
   }
 });
 
 // BUSQUEDA POR CATEGORIA
-app.get('productos/categoria/:CategoryID', async (req, res) => {
+app.get("productos/categoria/:CategoryID", async (req, res) => {
   try {
     const { CategoryID } = req.params;
-    const product = await Product.findAll({where: {CategoryID}});
-    product ? res.json(product) 
-            : res.status(404).json({ message: 'Producto no encontrado' });
+    const product = await Product.findAll({ where: { CategoryID } });
+    product
+      ? res.json(product)
+      : res.status(404).json({ message: "Producto no encontrado" });
   } catch (error) {
-    console.error('Error al obtener el producto:', error);
-    res.status(500).json({ error: 'Ocurrió un error al obtener el producto' });
+    console.error("Error al obtener el producto:", error);
+    res.status(500).json({ error: "Ocurrió un error al obtener el producto" });
   }
 });
 
 // BUSQUEDA POR QUERY
-app.get('/productos/buscar/:query', async (req, res) => {
+app.get("/productos/buscar/:query", async (req, res) => {
   try {
     const { query } = req.params;
     const products = await Product.findAll({
       where: {
         productName: {
-          [Op.like]: `%${query}%` // Usamos LIKE en lugar de ILIKE
-        }
-      }
+          [Op.like]: `%${query}%`, // Usamos LIKE en lugar de ILIKE
+        },
+      },
     });
 
     products.length
-      ? res.json(products) 
-      : res.status(404).json({ message: 'Producto no encontrado' });
+      ? res.json(products)
+      : res.status(404).json({ message: "Producto no encontrado" });
   } catch (error) {
-    console.error('Error al obtener los productos:', error);
-    res.status(500).json({ error: 'Ocurrió un error al obtener los productos' });
+    console.error("Error al obtener los productos:", error);
+    res
+      .status(500)
+      .json({ error: "Ocurrió un error al obtener los productos" });
   }
 });
 
 // BUSQUEDA POR IMPORTE MAYOR CON OPERADOR
-app.get('/productos/importeMayor/:query', async (req, res) => {
+app.get("/productos/importeMayor/:query", async (req, res) => {
   try {
     const { query } = req.params;
     const products = await Product.findAll({
       where: {
         UnitPrice: {
-          [Op.gt]: query
-        }},
-        //ORDENAR POR:
-        order: [['UnitPrice','ASC']]
+          [Op.gt]: query,
+        },
+      },
+      //ORDENAR POR:
+      order: [["UnitPrice", "ASC"]],
     });
-    products.length 
-      ? res.json(products) 
-      : res.status(404).json({ message: 'Producto no encontrado' });
+    products.length
+      ? res.json(products)
+      : res.status(404).json({ message: "Producto no encontrado" });
   } catch (error) {
-    console.error('Error al obtener los productos:', error);
-    res.status(500).json({ error: 'Ocurrió un error al obtener los productos' });
+    console.error("Error al obtener los productos:", error);
+    res
+      .status(500)
+      .json({ error: "Ocurrió un error al obtener los productos" });
   }
 });
 
-
-
 // Endpoint para obtener todos los empleados
-app.get('/empleados', async (req, res) => {
+app.get("/empleados", async (req, res) => {
   try {
     const employees = await Employees.findAll();
     res.json(employees);
   } catch (error) {
-    console.error('Error al obtener los empleados:', error);
-    res.status(500).json({ error: 'Ocurrió un error al obtener los empleados' });
+    console.error("Error al obtener los empleados:", error);
+    res
+      .status(500)
+      .json({ error: "Ocurrió un error al obtener los empleados" });
   }
 });
